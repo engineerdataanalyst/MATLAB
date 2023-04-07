ea5fb8e59dbee8a6b6dd9ceac1f26d09ecb97c37
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-07 10:10:20 -0400

    Update cellismember.m

--- 3.) logical functions/cellismember.m
+++ 3.) logical functions/cellismember.m
@@ -1,1 +1,24 @@
-% something
+function bool = cellismember(a, b)
+  % ----------------------------------------------
+  % - a slight variation of the ismember function
+  % - will return false for entries
+  %   of a cell array that are empty
+  %   and return the logical condition
+  %   computed by the regular ismember function
+  %   for the non-empty entries
+  % ----------------------------------------------
+  
+  %% check the input argument
+  % check the argument classes
+  arguments
+    a cell;
+    b;
+  end
+  %% find the location of the elements in 'a' that are in 'b'
+  if isempty(a)
+    bool = false;
+  else
+    emptys = cellfun(@isempty, a);
+    bool = false(size(a));
+    bool(~emptys) = ismember(a(~emptys), b);
+  end
