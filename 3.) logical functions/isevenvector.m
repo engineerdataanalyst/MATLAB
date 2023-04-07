49b06092b392888f90550ec6f486f6d1a9e61ac6
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-07 12:47:02 -0400

    Create isevenvector.m

---
+++ 3.) logical functions/isevenvector.m
@@ -0,0 +1,22 @@
+function bool = isevenvector(a, options)
+  % ---------------------------
+  % - returns true if an array
+  %   is an even integer vector
+  %   with a specific length
+  % ---------------------------
+  
+  %% check the input arguments
+  arguments
+    a;
+    options.Len (1,1) double {mustBeInteger, mustBeNonnegative};
+    options.Type ...
+    {mustBeTextScalar, ...
+     mustBeMemberi(options.Type, ["positive" ...
+                                  "positive or zero" ...
+                                  "negative" ...
+                                  "negative or zero"])};
+  end
+  %% check the array
+  Args = namedargs2cell(options);
+  bool = (isnumvector(a, Args{:}) || ...
+          issymvector(a, Args{:})) && isalleven(a);
