3459bafc549a818fbfdacbb1e6fd21264417eea6
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-07 14:00:10 -0400

    Create ispiecewisevector.m

---
+++ 3.) logical functions/ispiecewisevector.m
@@ -0,0 +1,15 @@
+function bool = ispiecewisevector(a, options)
+  % --------------------------
+  % - returns true if an array
+  %   is a piecewise vector
+  %   with a specific length
+  % --------------------------
+  
+  %% check the input arguments
+  arguments
+    a;
+    options.Len (1,1) double {mustBeInteger, mustBeNonnegative};
+  end
+  %% check the array
+  Args = namedargs2cell(options);
+  bool = issymvector(a, Args{:}) && isallpiecewise(a);
