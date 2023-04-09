0b5c778903eaf0455ae0ea0eb637c00f90494fe9
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-09 11:51:26 -0400

    Create issymvarmatrix.m

---
+++ 3.) logical functions/issymvarmatrix.m
@@ -0,0 +1,16 @@
+function bool = issymvarmatrix(a, options)
+  % -------------------------------
+  % - returns true if an array
+  %   is a symbolic variable matrix
+  %   with a specific dimension
+  % -------------------------------
+  
+  %% check the input arguments
+  arguments
+    a;
+    options.Dim (1,:) double ...
+    {mustBeNonempty, mustBeInteger, mustBeNonnegative};
+  end
+  %% check the array
+  Args = namedargs2cell(options);
+  bool = issymmatrix(a, Args{:}) && isallsymvar(a);
