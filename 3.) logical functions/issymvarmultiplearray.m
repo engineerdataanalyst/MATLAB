06be7884afae9cd9d272b85126eb083274c5c83d
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-09 11:51:57 -0400

    Create issymvarmultiplearray.m

---
+++ 3.) logical functions/issymvarmultiplearray.m
@@ -0,0 +1,23 @@
+function bool = issymvarmultiplearray(a, options)
+  % ------------------------------
+  % - returns true if an array
+  %   is a numeric multiple
+  %   of a symbolic variable array
+  %   with a specific dimension
+  % ------------------------------
+  
+  %% check the input arguments
+  arguments
+    a;
+    options.Dim (1,:) double ...
+    {mustBeNonempty, mustBeInteger, mustBeNonnegative};
+    options.CountZero (1,1) logical = false;
+  end
+  %% check the array
+  Args = namedargs2cell(options);
+  if ~isfield(options, 'Dim')
+    bool = issymarray(a) && isallsymvarmultiple(a, Args{:});
+  else
+    bool = issymarray(a, Args{1:2}) && ...
+           isallsymvarmultiple(a, Args{3:end});
+  end
