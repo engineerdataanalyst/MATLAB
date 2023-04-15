71d5f28598151119c81126df8a75181f5fc5afb5
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-14 20:44:24 -0400

    Create un.m

---
+++ 7.) table functions/8.) branches/un.m
@@ -0,0 +1,34 @@
+function val = un(x)
+  % ----------------------------
+  % - the unit step nan function
+  %   in piecewise form
+  % ----------------------------
+  narginchk(1,1);
+  if isTextArray(x, ["char" "string" "cell of char"])
+    x = Str2sym(x);
+  elseif issymfun(x)
+    convert2symfun = true;
+    args = argnames(x);
+    x = formula(x);
+  else
+    convert2symfun = false;
+  end  
+  val = x;
+  if isnumeric(x) || all(issymnum(x), 'all')
+    val(isAlways(x >= 0)) = 1;
+    val(isAlways(x < 0)) = nan;
+  elseif issym(x)
+    for k = 1:numel(x)
+      val(k) = piecewise(x(k) >= 0, 1);
+    end    
+  else
+    str = stack('input argument must be one of these:', ...
+                '------------------------------------', ...
+                '1.) a numeric array', ...
+                '2.) a symbolic array', ...
+                '3.) a symbolic string');
+    error(str);
+  end
+  if convert2symfun
+    val(args) = val;
+  end
