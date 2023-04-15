feed243ab92390f7ab9c8b1aae2cff0d8601fe73
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-15 18:54:22 -0400

    Create mustBeEqualLenScalar.m

---
+++ 11.) error handling functions/mustBeEqualLenScalar.m
@@ -0,0 +1,25 @@
+function mustBeEqualLenScalar(a, b)
+  if isTextScalar(a)
+    a = {a};
+  elseif issymfun(a)
+    a = formula(a);
+  end
+  if isTextScalar(a)
+    a = {a};
+  elseif issymfun(b)
+    b = formula(b);
+  end
+  if ~isequallen(a, b) && ~isscalar(a)
+    aname = inputname(1);
+    bname = inputname(2);
+    if isempty(aname)
+      aname = 'arg1';
+    end
+    if isempty(bname)
+      bname = 'arg2';
+    end
+    str = stack('', ...
+                '''%s'' must have the same length as ''%s''', ...
+                'or must be a scalar array');
+    error(str, aname, bname);
+  end
