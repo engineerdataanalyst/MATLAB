1b3022b19585faeff093b04575cd17739a148c41
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-14 20:38:42 -0400

    Create inrange.m

---
+++ 7.) table functions/8.) branches/inrange.m
@@ -0,0 +1,57 @@
+function val = inrange(x, range)
+  % -----------------------
+  % - the in range function
+  % -----------------------
+
+  %% check the input arguments
+  narginchk(2,2);
+  if issymfun(x)
+    x_is_symfun = true;
+    x_args = argnames(x);
+    x = formula(x);
+  else
+    x_is_symfun = false;
+  end
+  if issymfun(range)
+    range_is_symfun = true;
+    range_args = argnames(range);
+    range = formula(range);
+  else
+    range_is_symfun = false;
+  end
+  if x_is_symfun && range_is_symfun && ~isequal(x_args, range_args)
+    error(message('symbolic:symfun:InputMatch'));
+  end
+  if ~isnumeric(x) && ~issym(x)
+    str = stack('first argument must be', ...
+                'a numeric or symbolic expression');
+    error(str);
+  end
+  if ~isnumvector(range, 'Len', 2) && ~issymvector(range, 'Len', 2)
+    str = stack('second argument must be', ...
+                'a numeric or symbolic vector', ...
+                'of length 2');
+    error(str);
+  end
+  %% return the value
+  if isnumeric(x) && isnumeric(range)
+    val = x;
+    val(range(1) <= x & x <= range(2)) = 1;
+    val(range(1) > x | x > range(2)) = 0;
+  elseif (isnumeric(x) || all(issymnum(x), 'all')) && ...
+         (isnumeric(range) || all(issymnum(range), 'all'))
+    if issym(x)
+      x = double(x);
+    elseif issym(range)
+      range = double(range);
+    end
+    val = sym(inrange(x, range));
+  else
+    val = sym.zeros(size(x));
+    for k = 1:numel(x)
+      val(k) = piecewise(range(1) <= x(k) & x(k) <= range(2), 1, 0);
+    end
+    if x_is_symfun || range_is_symfun
+      val(x_args) = val;
+    end
+  end
