61a1eb011631bcb4201d62bbd59fb18d91f89f90
Chris Barnett <126098917+engineerdataanalyst@users.noreply.github.com>
2023-04-16 05:51:27 -0400

    Create fake_product.m

---
+++ 13.) math functions/5.) scalar equations/fake_product.m
@@ -0,0 +1,47 @@
+function [f g] = fake_product(g, x, options)
+  % --------------------------------
+  % - given a function (g),
+  %   computes a second function (f)
+  %   in the fake product rule
+  % - (f*g)' == f'*g'
+  % --------------------------------
+  
+  %% check the input argument
+  % check the argument classes
+  arguments
+    g sym;
+    x sym = default_var(g);
+    options.IgnoreAnalyticConstraints;
+    options.IgnoreSpecialCases;
+    options.PrincipalValue;
+    options.Hold;
+  end
+  % check the argument dimensions
+  if ~compatible_dims(g, x)
+    error('input arguments must have compatible dimensions');
+  end
+  % check the integraiton variable
+  if ~isallsymvar(x)
+    error('''x'' must be an array of symbolic variables');
+  end
+  % check the integration options
+  options = namedargs2cell(options);
+  %% compute the fake product rule function
+  IAC = {'IgnoreAnalyticConstraints' true};
+  dg = diff(g, x);
+  f = simplify(dg./(dg-g), IAC{:});
+  f = exp(int(f, options{:}));
+end
+% =
+function x = default_var(a)
+  % ----------------------------------
+  % - helper function for determining
+  %   the default integration variable
+  % ----------------------------------
+  if ~isallsymnum(a)
+    x = symvar(a, 1);
+  else
+    x = sym('x');
+  end
+end
+% =
